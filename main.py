ba7f26d0333295965f601aa08ac09286e5fbe7fb
datorot93 <herrion1@hotmail.com>
2022-01-31 22:42:31 -0500

    models_users

---
+++ main.py
@@ -0,0 +1,49 @@
+# Python
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+# FastAPI
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Models
+
+class UserBase(BaseModel):
+    user_ud: UUID = Field(...) #Universal Unid Identifier
+    email: EmailStr = Field(...)
+
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8
+    )
+
+
+class User(UserBase):
+    
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
+class Tweet(BaseModel):
+    pass
+
+
+@app.get(path="/")
+def home():
+    return {"Twitter API": "Working!"}
